e852f150539be155ea84d37d03bab41319596897
Nicholas Wilson <nicholaswilson97@gmail.com>
2026-03-04 16:11:27 -0800

    Work on lesson 15

---
+++ localusers/luser-demo04.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This script creates an account on the local system.
+# You will be prompted for the account name and password.
+
+# Ask for the user name.
+read -p 'Enter the username to create: ' username
+
+# Ask for the real name.
+
+# Ask for the password.
+
+# Create the user.
+
+# Set password for the user.
+
+# Force password change on first login.
+
+
