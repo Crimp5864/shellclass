f27cad444b9c550ba2923a789f8fa41d1681479d
Nicholas Wilson <nicholaswilson97@gmail.com>
2026-04-10 14:52:58 -0700

    Add disable-local-user.sh

---
+++ localusers/disable-local-user.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# Enforce execution as root or exit with error.
+# Provide a usage statement if no account name supplied and exit with error.
+# Default behavior should disable account.
+# Allow the following options:
+#	-d Deletes account instead of disabling.
+#	-r Removes the home directory.
+#	-a Creates an archive of the home directory and stores it in the /archives directory.
+#	Any other options should display usage and exit with error.
+# Accept list of usernames as arguments. At least one is required.
+# Refuse to disable or delete any account lower than 1000 UID.
+#	Only allow help desk to change user accounts, not system accounts.
+# Inform the user if account was not able to be disabled, deleted, or archived.
+# Display uername and any actions performed against the account.
+
+usage () {
+	# Print usage and exit with error.
+	read -r -d '' msg <<-EOF
+		Usage: ${0##*/} [-adr] USEERNAME
+		Disable account for USERNAME
+		  -a	Archive home directory
+		  -d	Delete account
+		  -r 	Remove home directory
+EOF
+	printf '%s\n' "$msg" >&2
+	exit 1
+}
+
+# Check if user is root and exit if not
+if [[ "$UID" -ne 0 ]]; then
+	echo 'Must be root' >&2
+	exit 1
+fi
+
+# Take username and full name as arguments
+username="$1"
+
+# Create user
+useradd --comment "$fullname" --create-home "$username" &> /dev/null
+
+# Check if useradd succeeded
+if [[ $? -ne 0 ]]; then
+	echo 'Account creation failed' >&2
+	exit 1
+fi
+
+# Create password
+passwd --stdin "$username" <<< "$password" &> /dev/null
+
+# Check if passwd succeeded
+if [[ $? -ne 0 ]]; then
+	echo 'Password creation failed' >&2
+	exit 1
+fi
+
+# Require password be changed on first login
+passwd -e "$username" &> /dev/null
+
+if [[ $? -ne 0 ]]; then
+	echo 'Account expiration failed' >&2
+	exit 1
+fi
+
+# Display username, password, and host
+cat <<-EOF
+	---
+	username:
+	$username
+
+	password:
+	$password
+
+	host:
+	$HOSTNAME
+	---
+EOF
+
+exit 0
---
+++ localusers/luser-demo12.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+# This script deletes a user.
+
+# Run as root.
+if [[ "$UID" -ne 0 ]]; then
+	echo 'Please run with sudo or as root.' >&2
+	exit 1
+fi
+
+# Assume the first argument is the user to delete.
+USER="$1"
+
+# Delete the user.
+userdel "$USER"
+
+# Make sure the user got deleted
+if [[ "$?" -ne 0 ]]; then
+	echo "The account $USER was NOT deleted." >&2
+	exit 1
+fi
+
+# Tell the user the account was deleted.
+echo "The account $USER was deleted."
+
+# Exit with success
+exit 0
+#!/usr/bin/env bash
+
+# This script generates a random password.
+# The user can set the password length with -l and add a special character with -s.
+# Verbose mode can be enabled with -v.
+
+usage () {
+	# Print usage and exit with error.
+	read -r -d '' msg <<-EOF
+		Usage: ${0##*/} [-vs] [-l LENGTH]
+		Generate a random password
+		  -l LENGTH Specify the password length.
+		  -s	    Append a special character to the password.
+		  -v	    Increase verbosity.
+EOF
+	printf '%s\n' "$msg" >&2
+	exit 1
+}
+
+log () {
+	# Print verbose messages to screen.
+	local message="$*"
+	if [[ "$verbose" = 'true' ]]; then
+		echo "$message" 
+	fi
+}
+
+# Set a default password length.
+length=48
+
+while getopts vl:s option; do
+	case "$option" in
+		v)
+			verbose='true'
+			log 'Verbose mode on.'
+			;;
+		l)
+			length="$OPTARG"
+			;;
+		s)
+			use_special_character='true'
+			;;
+		?)
+			usage
+			;;
+	esac
+done
+
+# Remove the options while leaving the remaining arguments.
+shift "$(( OPTIND - 1 ))"
+
+if [[ "$#" -gt 0 ]]; then
+	usage
+fi
+
+log 'Generating a password.'
+
+password=$(date +%s%N$RANDOM$RANDOM | sha256sum | head --bytes="$length")
+
+# Append a special character if requested to do so.
+if [[ "$use_special_character" = 'true' ]]; then
+	log 'Selecting a random special character.'
+	special_character=$(echo '!@#$%^&*()_-+=' | fold --width=1 | shuf | head --bytes=1)
+	password="$password$special_character"
+fi
+
+log 'Done.'
+log 'Here is the password:'
+
+# Display the password.
+echo "$password"
+
+exit 0
