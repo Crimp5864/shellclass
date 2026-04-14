413016cf2e45a350af0ca2e0d7abefade18ccd66
Nicholas Wilson <nicholaswilson97@gmail.com>
2026-04-14 13:16:16 -0700

    Lesson 41 complete

--- localusers/show-attackers.sh
+++ localusers/show-attackers.sh
@@ -6,11 +6,34 @@
 # Produce output in CSV with header of 'Count,IP,Location'.
 
 # Assign first argument to variable
-file=$1
+log_file="$1"
+limit='10'
 
 # Check that file exists or exit with error
 if [[ ! -e "$1" ]]; then
-	echo 'File not provided or cannot be read.' >&2
+	echo "Cannot open log file: $log_file" >&2
 	exit 1
 fi
+
+# Produce CSV output with headers 'Count,IP,Location'
+echo 'Count,IP,Location'
+
+# Loop through list of failed logins and IPs
+grep Failed syslog-sample | awk '{print $(NF - 3)}' | \
+       	sort | uniq -c | sort -nr | \
+	while read count ip; do
+		# If the number of attempts is greater than the limit
+		# display count, IP, and location.
+		if [[ "$count" -gt "$limit" ]]; then
+			location=$(geoiplookup $ip | awk -F ', ' '{print $2}')
+			echo "$count,$ip,$location"
+		fi
+	done
+	exit 0
+
+
+# List IPs that fail more than 10 times
+
+# For each IP list number of attempts made, IP, and location
+
 
